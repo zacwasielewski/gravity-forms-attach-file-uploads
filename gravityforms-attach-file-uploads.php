5efc284aabf9e2de5fd81d0510461907760a0958
Zac Wasielewski <zac@wasielewski.org>
2014-05-27 16:39:29 -0400

    Restructure plugin to use the Gravity Forms Add-On Framework

--- gravityforms-attach-file-uploads.php
+++ gravityforms-attach-file-uploads.php
@@ -1,72 +1,91 @@
 <?php
 /* 
 
-Plugin Name: Gravity Forms: Attach File Uploads
+Plugin Name: Gravity Forms Attach File Uploads
 Plugin URI: http://github.com/zacwasielewski/gravity-forms-attach-file-uploads/
-Description: Add-on for Gravity Forms to attach user-submitted File Upload data to notification emails
-Version: 0.1.2
+Description: An add-on for Gravity Forms to attach user-submitted File Upload data to notification emails
+Version: 0.2.0
 Author: Zac Wasielewski
 Author URI: http://wasielewski.org
 
 */
 
-if( class_exists('GFForms') ) {
-	add_filter("gform_notification_ui_settings", "attach_fileuploads_notification_ui_settings",        10, 3);
-	add_action("gform_pre_notification_save",    "attach_fileuploads_notification_ui_settings_save",   10, 2);
-	add_filter("gform_notification",             "attach_fileuploads_add_attachments_to_notification", 10, 3);
-}
+if (class_exists("GFForms")) {
+	GFForms::include_addon_framework();
 
-function attach_fileuploads_notification_ui_settings($ui_settings, $notification, $form) {
-	
-	ob_start();
-	?>
-	<tr valign="top">
-		<th scope="row">
-			<label for="gform_notification_attach_fileuploads">
-				<?php _e("Attachments", "gravityforms"); ?>
-			</label>
-		</th>
-		<td>
-			<input type="checkbox" name="gform_notification_attach_fileuploads" id="gform_notification_attach_fileuploads" value="1" <?php echo empty($notification["attachFileUploads"]) ? "" : "checked='checked'" ?>/>
-			<label for="gform_notification_attach_fileuploads" class="inline">
-				<?php _e("Attach File Upload data to notification email", "gravityforms"); ?>
-			</label>
-		</td>
-	</tr>
-	<?php $ui_settings['notification_attach_fileuploads'] = ob_get_contents(); ob_clean(); ?>
-	<?php
-	
-	return $ui_settings;
-}
+	class GFAttachFileUploads extends GFAddOn {
 
-function attach_fileuploads_notification_ui_settings_save($notification, $form) {
-	$notification['attachFileUploads'] = rgpost('gform_notification_attach_fileuploads');
-	return $notification;
-}
+		protected $_version = "0.2.0";
+		protected $_min_gravityforms_version = "1.7.9999";
+		protected $_slug = "gravity-forms-attach-file-uploads";
+		protected $_path = "zacwasielewski/gravity-forms-attach-file-uploads.php";
+		protected $_full_path = __FILE__;
+		protected $_title = "Gravity Forms Attach File Uploads";
+		protected $_short_title = "Attach File Uploads";
 
-function attach_fileuploads_add_attachments_to_notification($notification, $form, $entry){
-	
-	if ($notification['attachFileUploads'] != 1)
-		return $notification;
-	
-	$attachments = array();
-	$fileupload_fields = GFCommon::get_fields_by_type($form, array("fileupload"));
-	
-	if(!is_array($fileupload_fields))
-		return $notification;
+		public function init(){
+			parent::init();
+			add_filter("gform_notification_ui_settings", array($this, "notification_ui_settings"),        10, 3);
+			add_action("gform_pre_notification_save",    array($this, "notification_ui_settings_save"),   10, 2);
+			add_filter("gform_notification",             array($this, "add_attachments_to_notification"), 10, 3);
+		}
 
-	if (empty($notification['attachments'])) {
-		$notification['attachments'] = array();
+		function notification_ui_settings($ui_settings, $notification, $form) {
+	
+			ob_start();
+			?>
+			<tr valign="top">
+					<th scope="row">
+							<label for="gform_notification_attach_fileuploads">
+									<?php _e("Attachments", "gravityforms"); ?>
+							</label>
+					</th>
+					<td>
+							<input type="checkbox" name="gform_notification_attach_fileuploads" id="gform_notification_attach_fileuploads" value="1" <?php echo empty($notification["attachFileUploads"]) ? "" : "checked='checked'" ?>/>
+							<label for="gform_notification_attach_fileuploads" class="inline">
+									<?php _e("Attach File Upload data to notification email", "gravityforms"); ?>
+							</label>
+					</td>
+			</tr> <!-- / disable autoformat -->
+			<?php $ui_settings['notification_attach_fileuploads'] = ob_get_contents(); ob_clean(); ?>
+			<?php
+	
+			return $ui_settings;
+		}
+
+		function notification_ui_settings_save($notification, $form) {
+			$notification['attachFileUploads'] = rgpost('gform_notification_attach_fileuploads');
+			return $notification;
+		}
+
+		function add_attachments_to_notification($notification, $form, $entry){
+	
+			if ($notification['attachFileUploads'] != 1)
+				return $notification;
+	
+			$attachments = array();
+			$fileupload_fields = GFCommon::get_fields_by_type($form, array("fileupload"));
+	
+			if(!is_array($fileupload_fields))
+				return $notification;
+
+			if (empty($notification['attachments'])) {
+				$notification['attachments'] = array();
+			}
+
+			$upload_root = RGFormsModel::get_upload_root();
+			foreach($fileupload_fields as $field){
+				$url = $entry[$field["id"]];    
+				$attachment = preg_replace('|^(.*?)/gravity_forms/|', $upload_root, $url);
+				if($attachment){
+					$notification['attachments'][] = $attachment;
+				}           
+			}
+	
+			return $notification;
+		}
+	
 	}
 
-	$upload_root = RGFormsModel::get_upload_root();
-	foreach($fileupload_fields as $field){
-		$url = $entry[$field["id"]];    
-		$attachment = preg_replace('|^(.*?)/gravity_forms/|', $upload_root, $url);
-		if($attachment){
-			$notification['attachments'][] = $attachment;
-		}           
-	}
-	
-	return $notification;
+	new GFAttachFileUploads();
 }
